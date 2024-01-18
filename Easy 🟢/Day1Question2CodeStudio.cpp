b411e6d2aba527fe83a6b27733f67178f370b687
ItsUtkarshhh <110464977+ItsUtkarshhh@users.noreply.github.com>
2024-01-19 00:32:41 +0530

    Create Day1Question2CodeStudio.cpp

---
+++ Easy 🟢/Day1Question2CodeStudio.cpp
@@ -0,0 +1,28 @@
+// Question Link : https://www.codingninjas.com/studio/problems/square-root-integral_893351?topList=love-babbar-dsa-sheet-problems&problemListRedirection=true
+int binarySearch (int n) {
+    int s = 0;
+    int e = n;
+    long long int mid = s+ (e-s)/2;
+    long long int ans = -1;
+    while(s<=e) {
+        long long int square = mid*mid;
+        if(square == n) {
+            return mid;
+        }
+        if(square < n) {
+            ans = mid;
+            s = mid+1;
+        }
+        else {
+            e = mid-1;
+        }
+        mid = s+ (e-s)/2;
+    }
+    return ans;
+}
+
+int floorSqrt(int n)
+{
+    return binarySearch(n);
+    // Write your code here.
+}
