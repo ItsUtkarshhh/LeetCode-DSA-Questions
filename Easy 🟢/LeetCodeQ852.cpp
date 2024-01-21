34130786d5e200d1c26e6ee51a35a326af1be71e
ItsUtkarshhh <110464977+ItsUtkarshhh@users.noreply.github.com>
2024-01-22 03:09:47 +0530

    Update LeetCodeQ852.cpp

--- Easy 🟢/LeetCodeQ852.cpp
+++ Easy 🟢/LeetCodeQ852.cpp
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/peak-index-in-a-mountain-array/
+// Question Link : https://leetcode.com/problems/peak-index-in-a-mountain-array/
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
